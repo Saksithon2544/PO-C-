6670db875a3fec0e9757eb8a0eed4aebced609e6
Saksithon_2544 <saksithon.mt@technictrang.ac.th>
2025-03-13 10:53:20 +0700

    Remove unused using directives from Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
